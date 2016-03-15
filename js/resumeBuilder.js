cc4fbc66a3b3ba3ef614ecb27b84a2b4108bbacc
Yagi Kazuko <ahiru222@gmail.com>
2016-03-15 11:28:17 +0900

    string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// $("#main").append("Kazuko");
+var awesomeThoughts = "I am Kazuko and Iam AWESOME!";
+// console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
