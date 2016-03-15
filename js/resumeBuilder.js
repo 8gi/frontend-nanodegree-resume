1cacf4bdf6a8c5ab97156cf02edaa48eb6612a29
Yagi Kazuko <ahiru222@gmail.com>
2016-03-15 11:31:37 +0900

    Iam  -- I am

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 // $("#main").append("Kazuko");
-var awesomeThoughts = "I am Kazuko and Iam AWESOME!";
+var awesomeThoughts = "I am Kazuko and I am AWESOME!";
 // console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 $("#main").append(funThoughts);
